cb5b8d74a003e1c92c73814bec57ce38dd860691
Marisa Henry <mvatanya@gmail.com>
2019-05-20 14:35:29 -0700

    add big container to append each child meme container and update all variables

--- script.js
+++ script.js
@@ -7,20 +7,35 @@ window.addEventListener("DOMContentLoaded", () => {
   var buttonElement = document.getElementById("button-add-memes");
   buttonElement.addEventListener("click", function(event) {
     event.preventDefault();
-
+    //create a container to put every memes in
+    var memeContainer = document.createElement("div");
+    //create elements for each meme that will be appended in the memeContainer
+    var pTop = document.createElement("p");
+    var pBottom = document.createElement("p");
+    var img = document.createElement("img");
+    //create class for each elements
+    memeContainer.className = "meme-container";
+    pTop.className = "top-text";
+    pBottom.className = "bottom-text";
+    img.className = "img-input";
+    //put image source in the element that is created for image (img)
     var imageUrlInputElement = document.getElementById("image-input");
-    var imageTag = document.getElementsByTagName("img")[0];
-    imageTag.src = imageUrlInputElement.value;
+    img.src = imageUrlInputElement.value;
     imageUrlInputElement.value = "";
-
-    var bottomTextInputElement = document.getElementById("bottom-text-input");
-    var bottomMemeTextElement = document.getElementById("bottom-text");
-    bottomMemeTextElement.textContent = bottomTextInputElement.value;
-    bottomTextInputElement.value = "";
-
+    //put value of toptext in the element that is created for the top text (pTop)
     var topTextInputElement = document.getElementById("top-text-input");
-    var topMemeTextElement = document.getElementById("top-text");
-    topMemeTextElement.textContent = topTextInputElement.value;
+    pTop.textContent = topTextInputElement.value;
     topTextInputElement.value = "";
+    //put value of bottomtext in the element that is created for the bottom text (pBottom)
+    var bottomTextInputElement = document.getElementById("bottom-text-input");
+    pBottom.textContent = bottomTextInputElement.value;
+    bottomTextInputElement.value = "";
+    //append each element in to the meme container
+    memeContainer.appendChild(pTop);
+    memeContainer.appendChild(pBottom);
+    memeContainer.appendChild(img);
+    //append each meme container into the big memes container
+    var allMemeContainerElement = document.getElementById("all-meme-container");
+    allMemeContainerElement.appendChild(memeContainer);
   });
 });
